b7df20027b0e8f7c17ba18daabb11c86af26fb30
Guilherme Nascimento <brcontainer@yahoo.com.br>
2017-02-13 01:59:18 -0200

    Update Highlight.user.js

--- Highlight.user.js
+++ Highlight.user.js
@@ -1,18 +1,32 @@
 // ==UserScript==
 // @name         Highlight
 // @namespace    stackuserflow
-// @version      0.1
+// @version      0.1.1
 // @description  highlight new posts
 // @author       Guilherme Nascimento
-// @match        http://pt.stackoverflow.com/*
-// @match        http://stackoverflow.com/*
+// @match        *://stackapps.com/*
+// @match        *://askubuntu.com/*
+// @match        *://superuser.com/*
+// @match        *://serverfault.com/*
+// @match        *://stackoverflow.com/*
+// @match        *://stackexchange.com/*
+// @match        *://*.stackapps.com/*
+// @match        *://*.askubuntu.com/*
+// @match        *://*.superuser.com/*
+// @match        *://*.serverfault.com/*
+// @match        *://*.stackoverflow.com/*
+// @match        *://*.stackexchange.com/*
+// @match        *://mathoverflow.net/*
+// @match        *://mathoverflow.com/*
+// @match        *://*.mathoverflow.net/*
+// @match        *://*.mathoverflow.com/*
 // @grant        none
 // ==/UserScript==
 
 (function(doc) {
     'use strict';
 
-    var bgColor = '#FCA'; //Troque pela cor desejada
+    var bgColor = '#e1e1f1'; //Troque pela cor desejada
 
     function trigger() {
         var s = doc.createElement("style");
